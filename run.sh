9e7d5e74e3b787c1f14a28a2d73e215dd94493f3
Porawit Poboonma <porawit.p@bizidea.co.th>
2015-12-03 16:36:12 +0700

    now script is working like a charm

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 PREFIX=gitlab
 HTTP_PORT=10080
 SSH_PORT=10022
-SECRET_KEY=long-and-random-alpha-numeric-string
+SECRET_KEY=longAndRandomAlphaNumericString
 
 # ------------------------------------------------------
 
@@ -16,24 +18,44 @@ fi
 
 # ------------------------------------------------------
 
+# delete all existing container if any
+
+if [[ "`docker ps -aq --filter="name=$PREFIX-app"`" != "" ]]; then
+  echo "Removing existing $PREFIX container."
+  docker rm -f $PREFIX > /dev/null
+fi
+if [[ "`docker ps -aq --filter="name=$PREFIX-postgresql"`" != "" ]]; then
+  echo "Removing existing $PREFIX-postgresql container."
+  docker rm -f $PREFIX-postgresql > /dev/null
+fi
+if [[ "`docker ps -aq --filter="name=$PREFIX-redis"`" != "" ]]; then
+  echo "Removing existing $PREFIX-redis container."
+  docker rm -f $PREFIX-redis > /dev/null
+fi
+
+# ------------------------------------------------------
+
 # postgresql
+echo "Starting $PREFIX-postgresql."
 docker run -d \
   --name $PREFIX-postgresql \
   --env "DB_NAME=gitlabhq_production" \
   --env "DB_USER=gitlab" \
   --env "DB_PASS=password" \
   --volume $PWD/data/postgresql:/var/lib/postgresql \
-  sameersbn/postgresql:9.4-8
+  sameersbn/postgresql:9.4-8 > /dev/null
 
 # radis
-docker run -d
+echo "Starting $PREFIX-redis."
+docker run -d \
   --name $PREFIX-redis \
   --volume $PWD/data/redis:/var/lib/redis \
-  sameersbn/redis:latest
+  sameersbn/redis:latest > /dev/null
 
 # gitlab
-docker run -d
-  --name $PREFIX \
+echo "Starting $PREFIX."
+docker run -d \
+  --name $PREFIX-app \
   --link $PREFIX-postgresql:postgresql \
   --link $PREFIX-redis:redisio \
   --publish $SSH_PORT:22 \
@@ -42,4 +64,9 @@ docker run -d
   --env "GITLAB_SSH_PORT=$SSH_PORT" \
   --env "GITLAB_SECRETS_DB_KEY_BASE=$SECRET_KEY" \
   --volume $PWD/data/gitlab:/home/git/data \
-  sameersbn/gitlab:8.2.2
+  sameersbn/gitlab:8.2.2 > /dev/null
+
+IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+echo "-- Gitlab is now available at http://$IP:$HTTP_PORT/ and ssh on port $SSH_PORT."
+echo "-- For default user and password, please see at https://github.com/sameersbn/docker-gitlab#quick-start"
