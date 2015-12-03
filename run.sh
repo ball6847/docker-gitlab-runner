953d3358f887991d235c8e85f06a03cff7d8ee5c
Porawit Poboonma <porawit.p@bizidea.co.th>
2015-12-03 15:45:10 +0700

    make config more customizable

--- run.sh
+++ run.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
 PREFIX=gitlab
+HTTP_PORT=10080
+SSH_PORT=10022
+SECRET_KEY=long-and-random-alpha-numeric-string
+
+# ------------------------------------------------------
 
 cd `dirname $0`
+
+# override with local config
+if [[ -f "$PWD/config" ]]; then
+  source $PWD/config
+fi
+
+# ------------------------------------------------------
 
 # postgresql
 docker run -d \
   --name $PREFIX-postgresql \
-  --env 'DB_NAME=gitlabhq_production' \
-  --env 'DB_USER=gitlab'
-  --env 'DB_PASS=password' \
+  --env "DB_NAME=gitlabhq_production" \
+  --env "DB_USER=gitlab"
+  --env "DB_PASS=password" \
   --volume $PWD/data/postgresql:/var/lib/postgresql \
   sameersbn/postgresql:9.4-8
 
@@ -24,10 +36,10 @@ docker run -d
   --name $PREFIX \
   --link $PREFIX-postgresql:postgresql \
   --link $PREFIX-redis:redisio \
-  --publish 10022:22 \
-  --publish 10080:80 \
-  --env 'GITLAB_PORT=10080' \
-  --env 'GITLAB_SSH_PORT=10022' \
-  --env 'GITLAB_SECRETS_DB_KEY_BASE=long-and-random-alpha-numeric-string' \
+  --publish $SSH_PORT:22 \
+  --publish $HTTP_PORT:80 \
+  --env "GITLAB_PORT=$HTTP_PORT" \
+  --env "GITLAB_SSH_PORT=$SSH_PORT" \
+  --env "GITLAB_SECRETS_DB_KEY_BASE=$SECRET_KEY" \
   --volume $PWD/data/gitlab:/home/git/data \
   sameersbn/gitlab:8.2.2
