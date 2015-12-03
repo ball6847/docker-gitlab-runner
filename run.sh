966bb9773f7c623722b00d76445731db3f5549f5
Porawit Poboonma <porawit.p@bizidea.co.th>
2015-12-03 18:23:34 +0700

    add option to boot with docker

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ HTTP_PORT=10080
 SSH_PORT=10022
 SECRET_KEY=longAndRandomAlphaNumericString
 EXPOSE_HTTP_PORT=true
+START_WITH_DOCKER=false
 
 # ------------------------------------------------------
 
@@ -33,43 +34,65 @@ if [[ "`docker ps -aq --filter="name=$PREFIX-redis"`" != "" ]]; then
   echo "Removing existing $PREFIX-redis container."
   docker rm -f $PREFIX-redis > /dev/null
 fi
+if [[ "$START_WITH_DOCKER" = true ]]; then
+  START_WITH_DOCKER_CMD="--restart=always"
+fi
 
 # ------------------------------------------------------
-
 # postgresql
+
 echo "Starting $PREFIX-postgresql."
-docker run -d \
-  --name $PREFIX-postgresql \
-  --env "DB_NAME=gitlabhq_production" \
-  --env "DB_USER=gitlab" \
-  --env "DB_PASS=password" \
-  --volume $PWD/data/postgresql:/var/lib/postgresql \
-  sameersbn/postgresql:9.4-8 > /dev/null
 
+POSTGRESQL_CMD="
+  docker run -d
+    $START_WITH_DOCKER_CMD
+    --name $PREFIX-postgresql
+    --env "DB_NAME=gitlabhq_production"
+    --env "DB_USER=gitlab"
+    --env "DB_PASS=password"
+    --volume $PWD/data/postgresql:/var/lib/postgresql
+    sameersbn/postgresql:9.4-8
+"
+
+$POSTGRESQL_CMD > /dev/null
+
+# ------------------------------------------------------
 # radis
-echo "Starting $PREFIX-redis."
-docker run -d \
-  --name $PREFIX-redis \
-  --volume $PWD/data/redis:/var/lib/redis \
-  sameersbn/redis:latest > /dev/null
 
+echo "Starting $PREFIX-redis."
+
+REDIS_CMD="
+  docker run -d
+    $START_WITH_DOCKER_CMD
+    --name $PREFIX-redis
+    --volume $PWD/data/redis:/var/lib/redis
+    sameersbn/redis:latest
+"
+
+$REDIS_CMD > /dev/null
+
+# ------------------------------------------------------
 # gitlab
+
 echo "Starting $PREFIX-app."
 
 if [[ "$EXPOSE_HTTP_PORT" = true ]]; then
   EXPOSE_HTTP_PORT_CODE="--publish $HTTP_PORT:80 --env "GITLAB_PORT=$HTTP_PORT""
 fi
 
-GITLAB_CMD="docker run -d
-  --name $PREFIX-app
-  --link $PREFIX-postgresql:postgresql
-  --link $PREFIX-redis:redisio
-  --publish $SSH_PORT:22
-  $EXPOSE_HTTP_PORT_CODE
-  --env "GITLAB_SSH_PORT=$SSH_PORT"
-  --env "GITLAB_SECRETS_DB_KEY_BASE=$SECRET_KEY"
-  --volume $PWD/data/gitlab:/home/git/data
-  sameersbn/gitlab:8.2.2"
+GITLAB_CMD="
+  docker run -d
+    $START_WITH_DOCKER_CMD
+    $EXPOSE_HTTP_PORT_CODE
+    --name $PREFIX-app
+    --link $PREFIX-postgresql:postgresql
+    --link $PREFIX-redis:redisio
+    --publish $SSH_PORT:22
+    --env "GITLAB_SSH_PORT=$SSH_PORT"
+    --env "GITLAB_SECRETS_DB_KEY_BASE=$SECRET_KEY"
+    --volume $PWD/data/gitlab:/home/git/data
+    sameersbn/gitlab:8.2.2
+"
 
 $GITLAB_CMD > /dev/null
 
